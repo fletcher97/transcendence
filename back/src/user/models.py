fbe32c9d3d63053bfcc2918f9ff89cbc3b31b7ee
Israel Rifarachi hoyos <irifarac@car11s3.42barcelona.com>
2023-12-22 11:22:15 +0100

    modifying user table

--- back/src/user/models.py
+++ back/src/user/models.py
@@ -3,8 +3,12 @@ from django.db import models
 # Create your models here.
 
 class Users(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    birth_date = models.DateField()
     username = models.CharField(max_length=64)
     password = models.CharField(max_length=64)
+    created_date_time = models.DateField()
 
     def __str__(self):
         return (f"{self.username}")
