27e8b2bad4496d2ba163b004eb8303cbe6b781db
Miguel Gueifao <miguel.gueifao@gmail.com>
2024-01-01 21:12:35 +0100

    Changed url and added method check

--- back/src/pong/urls.py
+++ back/src/pong/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("get_rooms", views.get_rooms),
+    path("rooms", views.get_rooms),
 ]
--- back/src/pong/views.py
+++ back/src/pong/views.py
@@ -1,3 +1,4 @@
+from django.views.decorators.http import require_GET
 from django.http import JsonResponse
 
 from . import rooms
@@ -5,5 +6,6 @@ from . import rooms
 # Create your views here.
 
 
+@require_GET
 def get_rooms(_):
     return JsonResponse(rooms.Rooms.get_rooms(), safe=False)
