f72bbd0d8e05ae2173a30caa972245a81ca078be
David Bekic <itstijuanababy@gmail.com>
2024-02-02 15:53:26 +0100

    new socket service

---
+++ front/services/api/classicPongSocket.js
@@ -0,0 +1,22 @@
+import FRONT_DEV_ENV from "../../config.js";
+
+export const newClassicPongSocket = (roomName) => {
+  const socket = new WebSocket(
+    `${FRONT_DEV_ENV.WEB_SOCKET_URL}/ws/${roomName}`
+  );
+
+  return socket;
+};
+
+export const classicPongConnectionMessage = (socket, payload) => {
+  socket.send(JSON.stringify(payload));
+};
+
+export const classicPongKeyMessage = (socket, payload) => {
+    const key = event.key === "ArrowUp" ? 1 : -1;
+    const message = {
+      type: "movement",
+      key: key,
+    };
+    socket.send(JSON.stringify(message));
+};
